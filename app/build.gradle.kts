0eed25f4a11de579ff2291d86550af11c7801b11
Silga Salimata <silgasalmata0@gmail.com>
2026-04-13 19:47:05 +0000

    Ajout de la vérification de cohérence Coupure/Retour dans le formulaire

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.*
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.services)
@@ -5,19 +7,20 @@ plugins {
 
 android {
     namespace = "com.example.electricite"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.electricite"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -25,6 +28,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,16 +40,17 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 
+    // Firebase (BOM est la méthode recommandée)
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
 
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-    // --- LIBRAIRIE POUR LA CARTE (OSMDroid) ---
+    // Graphiques et Cartes
+    implementation("com.github.philjay:mpandroidchart:v3.1.0")
     implementation("org.osmdroid:osmdroid-android:6.1.18")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
